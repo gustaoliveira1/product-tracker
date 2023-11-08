ef5335ea5c824e5dd315b01720b8780b6b9c4920
Gustavo Oliveira <gusta01.oliveira@gmail.com>
2023-11-08 09:20:10 -0300

    feat: adding a simple error handling

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,21 @@ const PORT = 8080;
 
 const app = fastify();
 
-app.get('/products', async (request) => {
-    const querySchema = z.object({
-        name: z.string(),
-    });
+app.get('/products', async (request, reply) => {
+  try {
+      const querySchema = z.object({
+          name: z.string(),
+      });
 
-    const { name } = querySchema.parse(request.query);
+      const { name } = querySchema.parse(request.query);
 
-    const productScrapper = new ProductWebScrapping();
-    const products = productScrapper.call(name);
+      const productScrapper = new ProductWebScrapping();
+      const products = productScrapper.call(name);
 
-    return products;
+      return products;
+  } catch (error) {
+      reply.status(400).send({ error: 'Product name was not given' })
+  }
 });
 
 app.listen({ port: PORT }).then(() => {
