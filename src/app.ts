4bc3e61ac4e88fd7248a2bf150042257c985e5fa
Gustavo Oliveira <gusta01.oliveira@gmail.com>
2023-11-07 21:44:32 -0300

    feat: adding search product route

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log('hello world');
+import fastify from 'fastify';
+import { z } from 'zod';
+
+import { ProductWebScrapping } from './services/ProductWebScrapping';
+
+const PORT = 8080;
+
+const app = fastify();
+
+app.get('/products', async (request) => {
+    const querySchema = z.object({
+        name: z.string(),
+    });
+
+    const { name } = querySchema.parse(request.query);
+
+    const productScrapper = new ProductWebScrapping();
+    const products = productScrapper.call(name);
+
+    return products;
+});
+
+app.listen({ port: PORT }).then(() => {
+    console.info(`server listening on http://localhost:${PORT}`);
+});
